c5f6f8590590ece47455b5a040b5b15ea0bb5b6d
Chidera Ozigbo <82067806+Chideraozigbo@users.noreply.github.com>
2025-04-10 10:40:27 +0100

    Delete terraform/modules directory

--- terraform/modules/peering/output.tf
+++
@@ -1,9 +0,0 @@
-output "peering_connection_id" {
-  description = "The ID of the VPC peering connection"
-  value       = aws_vpc_peering_connection.peering.id
-}
-
-output "peering_connection_status" {
-  description = "The status of the VPC peering connection"
-  value       = aws_vpc_peering_connection.peering.accept_status
-}
